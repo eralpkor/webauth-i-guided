c1f83ff2747bc611da43e2d25652bb6736cc4ba8
eralpkor <eralp.ny@gmail.com>
2019-10-14 13:01:33 -0400

    this is stupid

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const cors = require('cors');
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
 
+// hash password
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync("B4c0/\/", salt);
+
 const server = express();
 
 server.use(helmet());
@@ -15,7 +20,7 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
-server.post('/api/register', (req, res) => {
+server.post('/api/register', (req, res) => { //
   let user = req.body;
 
   Users.add(user)
@@ -51,6 +56,26 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+server.get('/hash', (req, res) => {
+  // read a password from the Authorization header
+  // return an object with the password hashed using bcryptjs
+  // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
+  const creds = req.body;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash("B4c0/\/", salt, (err, hash) => {
+      res.send(201).json(hash)
+    })
+  })
+//   const credentials = req.body;
+
+// const hash = bcrypt.hashSync(credentials.password, 14);
+
+// credentials.password = hash;
+})
+
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
